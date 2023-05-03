d269c23024463c6eff9bd7c6700c33048d38e337
Anubhav pathak <88669266+Anubhav0114@users.noreply.github.com>
2023-05-03 19:06:07 +0530

    Update GameFragment.kt

--- app/src/main/java/com/example/trivia/GameFragment.kt
+++ app/src/main/java/com/example/trivia/GameFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
+
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
 class GameFragment : Fragment() {
